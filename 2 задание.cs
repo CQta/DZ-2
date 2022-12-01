3813da75c0ccd49c86ae1fec26d76e3823c5a5f7
CQta <awerin134@gmail.com>
2022-12-01 19:10:27 +0300

    3

--- 2 задание.cs
+++ 2 задание.cs
@@ -1,17 +1,8 @@
-﻿void Get_and_Print_Array() 
+﻿int x;
+int sum = 0;
+System.Console.WriteLine("Введите число");
+x = int.Parse(Console.ReadLine());
+for(;x!=0;x/=10)
 {
-   
-    int[] array = new int[8];
-    for(int i = 0;i<8;i++)
-    {
-        System.Console.WriteLine("Введите "+ i + "-ое число");
-        array[i] = int.Parse(Console.ReadLine());
-    }
-    for(int i = 0;i<8;i++)
-    {
-        System.Console.Write(array[i] + " ");
-    }
-   
+    
 }
-Get_and_Print_Array();
-
