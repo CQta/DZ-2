ea6f65fcd07f80f7d610f932590f1b32ae67b3f1
CQta <awerin134@gmail.com>
2022-12-01 18:17:34 +0300

    1

--- 2 задание.cs
+++ 2 задание.cs
@@ -1,10 +1,12 @@
-﻿System.Console.WriteLine("Введите кеш");
-double N = double.Parse(Console.ReadLine());
+﻿void pow(double a, double b) 
+{
+    double p = 0;
+    p = Math.Pow(a, b);
+    System.Console.WriteLine("a в степени b = ", p);
+}
+System.Console.WriteLine("Введите кеш");
+double a = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите годовой процент");
-double M = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите цена квартиры");
-double K = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите годовой процент квартиры");
-double Q = double.Parse(Console.ReadLine());
-int i = 0;
+double b = double.Parse(Console.ReadLine());
+pow(a, b);
 
