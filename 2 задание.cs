a1473b2a79d61b978de34b7d1967f512779212f0
CQta <awerin134@gmail.com>
2022-12-01 17:07:56 +0300

    2

--- 2 задание.cs
+++ 2 задание.cs
@@ -7,17 +7,4 @@ double K = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите годовой процент квартиры");
 double Q = double.Parse(Console.ReadLine());
 int i = 0;
-if (Q < M)
-{
-    while (N < K)
-    {
-        N = N + (N / (double)100 * M);
-        K = K + (K / (double)100 * Q);
-        i++;
-    }
-    Console.WriteLine("Жди " + i + " лет");
-}
-else
-{
-    Console.WriteLine("Квартиру не получешь");
-}
+
