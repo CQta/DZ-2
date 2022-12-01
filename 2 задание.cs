2f7f5202af666762eedc65625cc05cdbe758302f
CQta <awerin134@gmail.com>
2022-12-01 18:21:08 +0300

    1

--- 2 задание.cs
+++ 2 задание.cs
@@ -4,8 +4,8 @@
     p = Math.Pow(a, b);
     System.Console.WriteLine("a в степени b = ", p);
 }
-System.Console.WriteLine("Введите кеш");
+System.Console.WriteLine("Введите a");
 double a = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите годовой процент");
+System.Console.WriteLine("Введите b");
 double b = double.Parse(Console.ReadLine());
 pow(a, b);
