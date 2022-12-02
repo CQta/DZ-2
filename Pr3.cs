f28b94c166b3d584b56b6a8e8b7f449841a8772f
CQta <awerin134@gmail.com>
2022-12-02 20:30:28 +0300

    3-re

---
+++ Pr3.cs
@@ -0,0 +1,17 @@
+﻿void Get_and_Print_Array() 
+{
+   
+    int[] array = new int[8];
+    for(int i = 0;i<8;i++)
+    {
+        System.Console.WriteLine("Введите "+ i + "-ое число");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    for(int i = 0;i<8;i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+   
+}
+Get_and_Print_Array();
+
---
+++ Prog1.cs
@@ -0,0 +1,9 @@
+﻿int x;
+int sum = 0;
+System.Console.WriteLine("Введите число");
+x = int.Parse(Console.ReadLine());
+for(;x!=0;x/=10)
+{
+    sum += x%10;
+}
+System.Console.WriteLine("Сумма чисел = " + sum);
